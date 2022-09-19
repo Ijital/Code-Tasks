813d25e8410585c60f62b8ff7a7b5f1788baef9f
Melvin Ijih <meliji@hotmail.com>
2022-09-19 14:30:41 +0100

    third push

---
+++ String-Manipulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation
+{
+    internal class Program
+    {
+       
+
+       static int[] nums = new int[] { 1, 2, 3, 4, 5 };
+        static void Main(string[] args)
+        {
+            var semba = names.OrderBy(s=>s);
+
+            Debugger.Break();
+            Console.ReadLine();
+
+        }
+    }
+}
